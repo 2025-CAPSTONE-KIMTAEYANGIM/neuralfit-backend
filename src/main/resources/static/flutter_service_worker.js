04e13d651471f2971514c0b7b722cbc2b2a93ef2
KTH <pni2396@gmail.com>
2025-12-01 01:32:57 +0900

    fix: frontend api url modified

--- src/main/resources/static/flutter_service_worker.js
+++ src/main/resources/static/flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "07ca18bf53700051eeaac982d1f07c9e
 "canvaskit/skwasm.wasm": "1c93738510f202d9ff44d36a4760126b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "862c50d40b3122a3c690f19425267cae",
+"flutter_bootstrap.js": "afa250357e27be1b6183924fad63999a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
